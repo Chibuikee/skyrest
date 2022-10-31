2b2ee290f6ff76fdbc7500c084fe48cedce088e9
Chibuike Ewenike <sopewenike@yahoo.com>
2022-10-31 20:19:49 +0100

    css started

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { links, NavMenuList } from "./Navbardata";
 import { HiMenu } from "react-icons/hi";
 function Navbar() {
+  const [navBarToggle, setNavBarToggle] = useState(null);
+  //   useEffect(() => {
+  //     const w = () => {
+  //       window.innerWidth === 800 && setNavBarToggle(true);
+  //     };
+  //     window.addEventListener("resize", w);
+  //     return () => {
+  //       window.removeEventListener("resize", w);
+  //     };
+  //   }, []);
+  const navtogglerStyle = {
+    transform: `translate(${navBarToggle ? "-70%" : "50%"})`,
+  };
   return (
     <nav className="nav">
       <h1>SKYREST</h1>
       <div className="Nav-ctn">
-        <div className="Flex menuToggle">
+        <div
+          onClick={() => setNavBarToggle(!navBarToggle)}
+          className="Flex menuToggle"
+        >
           <h4>MENU</h4>
           <HiMenu />
         </div>
-        <div className="Nav-main">
+        <div style={navtogglerStyle} className="Nav-main">
           <div className="Nav-heading Flex">
-            <h3>NAVIGATION</h3> <span>x</span>
+            <h3>NAVIGATION</h3>
+            <span onClick={() => setNavBarToggle(!navBarToggle)}>x</span>
           </div>
           <ul className="Nav-menu">
             {NavMenuList.childList.map((item) => (
@@ -21,7 +38,12 @@ function Navbar() {
               </li>
             ))}
           </ul>
-          <ul className="Nav-social">
+          <p>
+            Perspiciatis hic praesentium nesciunt. Et neque a dolorum voluptatem
+            porro iusto sequi veritatis libero enim. Iusto id suscipit veritatis
+            neque reprehenderit.
+          </p>
+          <ul className="Nav-social Flex">
             {links.map((item, key) => (
               <li
                 key={key}
@@ -32,7 +54,7 @@ function Navbar() {
                   className=""
                 >
                   <svg
-                    className="w-6 h-6 text-[#afb5bd] fill-current "
+                    className="w-6 h-6 fill-current "
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox={item.box}
                   >
